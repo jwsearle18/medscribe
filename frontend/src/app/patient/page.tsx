61df380cb48054e29839d06b0ceec6c897a36865
sboris001 <nil.connections.sandbox@gmail.com>
2025-04-05 18:27:41 -0600

    linting

--- frontend/src/app/patient/page.tsx
+++ frontend/src/app/patient/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import Patient from "../../components/patient_profile/Patient";
 import Header from "../../components/global/Header"
 export default function Home() {
@@ -5,7 +7,9 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex flex-col items-center flex-1 bg-slate-800 pt-20 bg-pattern">
-        <Patient />
+        <Suspense fallback={<div>Loading patient profile...</div>}>
+          <Patient />
+        </Suspense>
       </main>
     </div>
   );
